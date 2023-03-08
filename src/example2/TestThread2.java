540f77a9db99d8e6be573ff6480c6608ac34172b
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2023-03-07 20:13:01 -0500

    Updated
    Removed unused method

--- src/example2/TestThread2.java
+++ src/example2/TestThread2.java
@@ -2,32 +2,24 @@ package example2;
 
 public class TestThread2 {
 	
-	private static int a = 10;
-	private static int b = -9;
-	
+
 	public static void main(String[] args) throws InterruptedException {
 		
 		Share share = new Share(3);
 		
-		Thread thread_A = new ThreadExtension(share,"Thread_A");
-		Thread thread_B = new ThreadExtension(share,"Thread_B");
-		
+		Thread thread_A = new ThreadExtension(share,"Thread_AAA");
+		Thread thread_B = new ThreadExtension(share,"Thread_BBB");
 		
 		
 		thread_A.start();
 		thread_B.start();
 		
-		System.out.print("1 a: "+ String.valueOf(a).toString() + "\n");
-		System.out.print("1 b: "+ String.valueOf(b).toString() + "\n");
+		System.out.print(" a: "+ String.valueOf(share.x).toString() + "\n");
+		System.out.print(" b: "+ String.valueOf(share.y).toString() + "\n");
 		
 		thread_A.join();
 		thread_B.join();
 	
-		System.out.print("shared secret: " + share.getSecret());
-	}
-	
-	public static void my_methdod() {
-		a = a+5;
-		b = b+5;
+		System.out.print("shared static secret: " + share.getClassSecret());
 	}
 }
